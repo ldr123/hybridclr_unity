5419c07cc9629a54922a3da289ed2e4b25f823a4
ldr123 <ldr12@163.com>
2022-10-17 12:08:44 +0800

    重新设置git同步 拉取完毕后直接设置tag

--- merge.upstream.bat
+++ merge.upstream.bat
@@ -1,4 +1,15 @@
+@echo off
+
 git checkout .
 git fetch upstream
 git merge upstream/main
 git push
+
+sed -n '/version/p' package.json > tmp.txt
+sed -i 's/[version,",: ]*//g' tmp.txt
+for /f "delims=" %%i in (tmp.txt) do set var=%%i
+echo 最新版本号:%var%
+del tmp.txt
+
+git tag %var%
+git push origin --tags
--- push-tag.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set /p var= ‰»Î∞Ê±æ∫≈:
-git tag %var%
-git push origin --tags
-pause
